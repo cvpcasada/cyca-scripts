82a5446bbf188671974956d90d3d92c5c5d57dbe
Cyrus Venn Casada <cvpcasada@outlook.com>
2019-04-15 15:49:38 +0800

    make edgy browsers the default

--- packages/babel-presets/create-defaults.js
+++ packages/babel-presets/create-defaults.js
@@ -1,5 +1,8 @@
 const _ = require('lodash/fp');
-const path = require('path');
+
+const safeTargets = {
+  browsers: ['>0.2%', 'not dead', 'not ie <= 11', 'not op_mini all'],
+};
 
 function createDefaults(opts) {
   const env = require('./env');
@@ -10,39 +13,38 @@ function createDefaults(opts) {
   let defaults = {
     env: {
       targets: {
-        browsers: isEnvDevelopment ? [
+        browsers: [
           'Safari >= 11.1',
           'iOS >= 11.4',
           'Edge >= 16',
           'Chrome >= 66',
           'ChromeAndroid >= 66',
           'Firefox >= 60',
-        ] : ['>0.2%', 'not dead', 'not ie <= 11', 'not op_mini all']
+        ],
       },
       exclude: [
         'transform-regenerator',
         'transform-async-to-generator',
-        'transform-typeof-symbol',  
+        'transform-typeof-symbol',
       ].filter(Boolean),
-      useBuiltIns: false
+      useBuiltIns: false,
     },
     runtime: {
       corejs: false,
       helpers: false,
       regenerator: false,
-      useESModules: isEnvDevelopment || isEnvProduction
+      useESModules: isEnvDevelopment || isEnvProduction,
     },
-    lodash: { "id": ['lodash', 'ramda', 'rambda', 'recompose', 'polished'] },
+    lodash: { id: ['lodash', 'ramda', 'rambda', 'recompose', 'polished'] },
     transformDefine: {
       __prod__: isEnvProduction,
       __PRODUCTION__: isEnvProduction,
       __dev__: isEnvDevelopment,
-      __DEVELOPMENT__: isEnvDevelopment
-    }
+      __DEVELOPMENT__: isEnvDevelopment,
+    },
   };
 
   return _.defaultsDeep(defaults, opts || {});
-
 }
 
-module.exports = createDefaults;
+module.exports = Object.assign(createDefaults, { safeTargets });
